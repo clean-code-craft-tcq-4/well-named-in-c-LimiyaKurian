1b35c48d2eafd9c1bd5f238a3d905ede71f9b389
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-14 00:46:51 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,100 +1,37 @@
 #include <stdio.h>
 #include <assert.h>
+#include <ColorCodePair.h>
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
-}
-
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
-
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
-{
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
-}
-
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
-
-void CodePairReference()
-{
-    int i,j;
-    ColorPair colorpairreference;
-	char colorPairNamesManuel[MAX_COLORPAIR_NAME_CHARS];
+int main() {
+    
+    ColorCode_ReferenceManuel();
 	
-    for(i=0;i<numberOfMajorColors;i++)
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    return 0;
+}
+
+void ColorCode_ReferenceManuel()
+{
+    int majorColorCount,minorColorCount;
+    ColorPair colorpairreference;
+    char colorPairNamesManuel[MAX_COLORPAIR_NAME_CHARS];
+	
+    for(majorColorCount=0;majorColorCount<numberOfMajorColors;majorColorCount++)
     {
-        for(j=0;j<numberOfMinorColors;j++)
+        for(minorColorCount=0;minorColorCount<numberOfMinorColors;minorColorCount++)
         {
-            colorpairreference.majorColor = (enum MajorColor)i;
-            colorpairreference.minorColor = (enum MinorColor)j;
+            colorpairreference.majorColor = (enum MajorColor)majorColorCount;
+            colorpairreference.minorColor = (enum MinorColor)minorColorCount;
             int pairnumberReference = GetPairNumberFromColor(&colorpairreference);
-			ColorPairToString(&colorpairreference, colorPairNamesManuel);
-            printf("ColorPairCodeReference  %s\n%d\n", colorPairNamesManuel,pairnumberReference );
+	    ColorPairToString(&colorpairreference, colorPairNamesManuel);
+	    printf("ColorPairCodeReferenceManuel\n");
+            printf(" %s\t%d\n", colorPairNamesManuel,pairnumberReference );
 
         }
     }
-}
-
-int main() {
-    //testNumberToPair(4, WHITE, BROWN);
-    //testNumberToPair(5, WHITE, SLATE);
-
-    //testPairToNumber(BLACK, ORANGE, 12);
-    //testPairToNumber(VIOLET, SLATE, 25);
-    
-    CodePairReference();
-
-    return 0;
 }
