404a832da693985472d09c2bda14b32239f6b4bb
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-14 21:29:42 +0530

    Add files via upload

---
+++ ColorCode_Main.h
@@ -0,0 +1,8 @@
+#include "ColorCode.h"
+
+//extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+//extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+void colorCodeReferenceManuel(void);
