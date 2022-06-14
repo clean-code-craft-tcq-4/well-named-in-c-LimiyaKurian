da4d5c895dda7441dd9de9d4de92eaec8e1c0998
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-14 21:12:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include "ColorCode.h"
 
 int main() {
-    
-    colorCode_ReferenceManuel();
 	
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
@@ -14,23 +12,3 @@ int main() {
     return 0;
 }
 
-void colorCode_ReferenceManuel(void)
-{
-    int majorColorCount,minorColorCount;
-    ColorPair colorpairreference;
-    char colorPairNamesManuel[MAX_COLORPAIR_NAME_CHARS];
-	
-    for(majorColorCount=0;majorColorCount<numberOfMajorColors;majorColorCount++)
-    {
-        for(minorColorCount=0;minorColorCount<numberOfMinorColors;minorColorCount++)
-        {
-            colorpairreference.majorColor = (enum MajorColor)majorColorCount;
-            colorpairreference.minorColor = (enum MinorColor)minorColorCount;
-            int pairnumberReference = GetPairNumberFromColor(&colorpairreference);
-	    ColorPairToString(&colorpairreference, colorPairNamesManuel);
-	    printf("ColorPairCodeReferenceManuel\n");
-            printf(" %s\t%d\n", colorPairNamesManuel,pairnumberReference );
-
-        }
-    }
-}
