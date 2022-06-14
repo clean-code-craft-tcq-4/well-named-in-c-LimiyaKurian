ce5d602f5941698ff361eec577bce446896e2d5b
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-15 00:53:51 +0530

    Delete ColorCode_Main.h

--- ColorCode_Main.h
+++
@@ -1,7 +0,0 @@
-
-extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
-void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
-void colorCodeReferenceManuel(void);
