d9b21196ee9261d0911694cad670363ba8d15cca
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-14 19:24:39 +0530

    Update ColorCode.h

--- ColorCode.h
+++ ColorCode.h
@@ -12,7 +12,7 @@ const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+//const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
