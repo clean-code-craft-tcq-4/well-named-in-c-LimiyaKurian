ccdffb7ec9b0feb13ac3a30129903091ba5e34f3
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-15 02:48:07 +0530

    Update ColorCode_Main.h

--- ColorCode_Main.h
+++ ColorCode_Main.h
@@ -1,9 +1,32 @@
-#include "ColorCode.h"
+typedef enum
+{
+   WHITE =0, 
+   RED,
+   BLACK, 
+   YELLOW,
+   VIOLET
+ }MajorColor;
+ 
+typedef enum
+{
+   BLUE=0, 
+   ORANGE,
+   GREEN, 
+   BROWN,
+   SLATE
+} MinorColor;
 
-MajorColor majorcolor;
-MinorColor minorcolor;
-ColorPair colorpairExtern;
-void testNumberToPair(int pairNumber,MajorColor expectedMajor,MinorColor expectedMinor);
-void testPairToNumber(MajorColor major,MinorColor minor,int expectedPairNumber);
+typedef struct {
+    MajorColor majorColor;
+    MinorColor minorColor;
+} ColorPair;
+
+extern ColorPair colorpairExtern;
+extern MajorColor majorcolor;
+extern MinorColor minorcolor;
+
+
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
 void colorCodeReferenceManuel(void);
 
