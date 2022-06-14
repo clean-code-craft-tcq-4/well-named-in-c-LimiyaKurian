9dcaaea16a842b123f1e98b7fa23c0cd8d1e16cb
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-15 01:37:51 +0530

    Update ColorCode.h

--- ColorCode.h
+++ ColorCode.h
@@ -1,5 +1,5 @@
-typedef enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-typedef enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
