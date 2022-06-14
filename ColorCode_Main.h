444094f6e1063d55edb89dab64e9d2cc3b6dc863
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-15 02:22:06 +0530

    Update ColorCode_Main.h

--- ColorCode_Main.h
+++ ColorCode_Main.h
@@ -1,7 +1,9 @@
 #include "ColorCode.h"
 
+extern enum MajorColor;
+extern enum MinorColor;
 ColorPair colorpairExtern;
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
-void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+void testNumberToPair(int pairNumber,MajorColor expectedMajor,MinorColor expectedMinor);
+void testPairToNumber(MajorColor major,MinorColor minor,int expectedPairNumber);
 void colorCodeReferenceManuel(void);
 
