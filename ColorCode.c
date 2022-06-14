4b931858aa0b3850c562550413e98546821ea372
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-15 03:04:09 +0530

    Update ColorCode.c

--- ColorCode.c
+++ ColorCode.c
@@ -2,8 +2,6 @@
 #include <assert.h>
 #include "ColorCode.h"
 
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
