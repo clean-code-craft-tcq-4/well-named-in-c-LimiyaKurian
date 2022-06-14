b76a2fe42b9d7c511584ae4c19c8a862d5ee8114
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-15 02:47:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -13,3 +13,26 @@ int main() {
     return 0;
 }
 
+void testNumberToPair(int pairNumber,
+    MajorColor expectedMajor,
+    MinorColor expectedMinor)
+{
+    colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(
+    MajorColor major,
+    MinorColor minor,
+    int expectedPairNumber)
+{
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
